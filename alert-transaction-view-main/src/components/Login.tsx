08e873011795a8ccc174948d381dbe69743f821a
satyjitdeshmukh <satyjit.deshmukh22@vit.edu>
2025-06-29 13:15:02 +0530

    Initial commit

--- alert-transaction-view-main/src/components/Login.tsx
+++ alert-transaction-view-main/src/components/Login.tsx
@@ -158,7 +158,7 @@ const Login = () => {
     }
     setIsLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch("https://looper-task1.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
--- alert-transaction-view-main/src/components/Register.tsx
+++ alert-transaction-view-main/src/components/Register.tsx
@@ -43,7 +43,7 @@ const Register = () => {
 
     setIsLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/register", {
+      const res = await fetch("https://looper-task1.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
